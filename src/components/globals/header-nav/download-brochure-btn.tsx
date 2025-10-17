484d524f1c738ab9f967d36793dd4b00a8bfdbd2
Aamir | ecodeshub <ecodeshub@gmail.com>
2025-10-17 11:08:51 +0530

    brochure download added

--- src/components/globals/header-nav/download-brochure-btn.tsx
+++ src/components/globals/header-nav/download-brochure-btn.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 const DownloadBrochureBtn= () => {
   return (
-    <Link href={"/"} className="bg-orange px-5 py-2.5 text-sm text-white rounded-sm font-semibold transition hover:bg-black">
+    <Link href="jamisu-brochure.pdf" className="bg-orange px-5 py-2.5 text-sm text-white rounded-sm font-semibold transition hover:bg-black">
       Download Brochure
     </Link>
   )
