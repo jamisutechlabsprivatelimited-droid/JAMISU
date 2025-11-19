250c09053813ed8e1d7757dbfa88bdec6b2068ee
surbhi9104 <surbhi@ignitivelabs.in>
2025-11-19 19:09:25 +0530

    Update about-us.tsx
    update

--- src/app/_components/about-us.tsx
+++ src/app/_components/about-us.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import JourneyImage from "@/assets/journey-image.png";
+// import Image from "next/image";
+// import JourneyImage from "@/assets/journey-image.png";
 
 
 const AboutUs = () => {
