11a612faf77e906a1358180a750f5dbc08b23002
surbhi9104 <surbhi@ignitivelabs.in>
2025-11-19 18:26:23 +0530

    Update hero.tsx
    changes align

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -28,7 +28,9 @@ const Hero = () => {
           </div>
 
           <div className="mt-5">
-            <Link href={"/contact-us"} className="text-blue bg-yellow px-4 py-2 font-medium rounded-md">
+            <a href="https://app.jamisu.com/"  /* change link here */
+            target="_blank"
+            rel="noopener noreferrer" className="text-blue bg-yellow px-4 py-2 font-medium rounded-md">
               JAMISU App
             </Link>
           </div>      
