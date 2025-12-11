10560f6474eb97e9c73d9dbdd674757a9b08da91
Jamisu <jamisutechlabsprivatelimited@gmail.com>
2025-12-11 19:25:27 +0530

    Update download-brochure-btn.tsx
    Fix download brochure

--- src/components/globals/header-nav/download-brochure-btn.tsx
+++ src/components/globals/header-nav/download-brochure-btn.tsx
@@ -2,7 +2,8 @@ import Link from "next/link"
 
 const DownloadBrochureBtn= () => {
   return (
-    <Link href="public/jamisu-brochure.pdf" className="bg-blue px-5 py-2.5 text-sm text-yellow rounded-sm font-semibold transition hover:bg-black">
+    <Link  href="public/jamisu-brochure.pdf"
+      target="_blank" className="bg-blue px-5 py-2.5 text-sm text-yellow rounded-sm font-semibold transition hover:bg-black">
       Download Brochure
     </Link>
   )
